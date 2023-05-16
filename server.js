cc51e2eb4062246c9e7ca90018976d1a7510340d
Joao Marchete <elisamarchete@gmail.com>
2023-05-16 18:37:00 -0400

    added path to create routes for index.html and notes.html

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // Import Express.js
 const express = require("express");
 const db = require("./db/db.json");
+const path = require("path");
 // Initialize an instance of Express.js
 const app = express();
 
 // Specify on which port the Express.js server will run
-const PORT = 3001;
+const PORT = 3002;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,15 +16,41 @@ app.use(express.static("public"));
 
 // Create Express.js routes for default '/', '/Notes'
 app.get("/", (req, res) => {
-  res.sendFile("./public/index.html");
+  //console.log(__dirname);
+  //console.log(path.join(__dirname, "public/first.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
+  // Show the user agent information in the terminal
+  // console.info(req.rawHeaders);
+
+  // Log our request to the terminal
+  // console.info(`${req.method} request received`);
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile("./public/notes.html");
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+  // Show the user agent information in the terminal
+  // console.info(req.rawHeaders);
+
+  // Log our request to the terminal
+  // console.info(`${req.method} request received`);
 });
 
 // path to access notes saved by user
-app.get("/api/notes", (req, res) => res.json(db)); // Maybe need to change path /api
+app.get("/api/notes", (req, res) => {
+  res.json(db); // Maybe need to change path /api
+});
+
+// POST request
+app.post("/api/notes", (req, res) => {
+  // Let the client know that their POST request was received
+  res.json(`${req.method} request received`);
+
+  // Show the user agent information in the terminal
+  // console.info(req.rawHeaders);
+
+  // Log our request to the terminal
+  // console.info(`${req.method} request received`);
+});
 
 // listen() method is responsible for listening for incoming connections on the specified port
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
